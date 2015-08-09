522c1f7430a8e7a71bfab42f1b61e72f88be952c
Matt Harrington <mattharrington@users.noreply.github.com>
2015-08-08 19:16:08 -0700

    PledgePage complete

--- XFStreamingAudio/PledgePage.xaml.cs
+++ XFStreamingAudio/PledgePage.xaml.cs
@@ -10,7 +10,48 @@ namespace XFStreamingAudio
         public PledgePage()
         {
             InitializeComponent();
+            donateBtn.Clicked += DonateBtn_Clicked;
+            buildingCampaignBtn.Clicked += BuildingCampaignBtn_Clicked;
+            volunteerBtn.Clicked += VolunteerBtn_Clicked;
+            membershipBtn.Clicked += MembershipBtn_Clicked;
+            vehicleDonationBtn.Clicked += VehicleDonationBtn_Clicked;
+            plannedGiftsBtn.Clicked += PlannedGiftsBtn_Clicked;
+            underwriterBtn.Clicked += UnderwriterBtn_Clicked;
+        }
+
+        void UnderwriterBtn_Clicked (object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/content/become-underwriter"));
+        }
+
+        void PlannedGiftsBtn_Clicked (object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/content/planned-gifts"));
+        }
+
+        void VehicleDonationBtn_Clicked (object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/content/vehicle-donation"));
+        }
+
+        void MembershipBtn_Clicked (object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/content/membership"));
+        }
+
+        void VolunteerBtn_Clicked (object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/content/volunteer"));
+        }
+
+        void BuildingCampaignBtn_Clicked (object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/content/bridge-street-project"));
+        }
+
+        void DonateBtn_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://www.kvmr.org/donate"));
         }
     }
 }
-
