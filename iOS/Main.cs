677a41eff9c2cad9a19ba83de49cfbb15443aaa2
Matt Harrington <mattharrington@users.noreply.github.com>
2015-09-08 15:58:12 -0700

    Added Xamarin.Insights

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using Xamarin;
 
 namespace XFStreamingAudio.iOS
 {
@@ -12,10 +13,10 @@ namespace XFStreamingAudio.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            Insights.Initialize("6177fa9abb40234e5363a7143c5ee7b9cf7a70a9");
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
 }
-
