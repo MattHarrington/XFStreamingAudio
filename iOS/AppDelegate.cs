93eb8dbeb067f7afda14a8742ab4049ceb98bb77
Matt Harrington <mattharrington@users.noreply.github.com>
2015-09-16 21:07:22 -0700

    Closes #22

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Diagnostics;
 using Foundation;
 using UIKit;
+using AI.XamarinSDK.Abstractions;
 
 namespace XFStreamingAudio.iOS
 {
@@ -14,6 +15,10 @@ namespace XFStreamingAudio.iOS
         {
             global::Xamarin.Forms.Forms.Init();
 
+            ApplicationInsights.Setup("6bc59a82-3838-4b99-806e-d6929b5b5651");
+            ApplicationInsights.Start();
+            AI.XamarinSDK.iOS.ApplicationInsights.Init();
+
             // Code for starting up the Xamarin Test Cloud Agent
             #if ENABLE_TEST_CLOUD
 			Xamarin.Calabash.Start();
@@ -23,6 +28,11 @@ namespace XFStreamingAudio.iOS
 
             return base.FinishedLaunching(app, options);
         }
+
+        public override void ReceiveMemoryWarning(UIApplication application)
+        {
+            Debug.WriteLine("ReceiveMemoryWarning()");
+            TelemetryManager.TrackEvent("ReceiveMemoryWarning()");
+        }
     }
 }
-
