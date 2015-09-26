982b7f5509e6260d9a57d0eac093f5b898827d14
Matt Harrington <mattharrington@users.noreply.github.com>
2015-09-26 00:46:13 -0700

    Closes #29

--- iOS/Main.cs
+++ iOS/Main.cs
@@ -13,7 +13,12 @@ namespace XFStreamingAudio.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            #if DEBUG
+            Insights.Initialize(Insights.DebugModeKey);
+            #else
             Insights.Initialize("6177fa9abb40234e5363a7143c5ee7b9cf7a70a9");
+            #endif
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
